01ecf9704817896fab6aadf7007d550ec01a178c
ruslan_lomaka <ruslan.lomaka@bini.games>
2021-03-12 17:54:06 +0200

    Add presentation proj

---
+++ Assets/ProjectAssets/Scripts/RotationScale.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScale : MonoBehaviour
+{
+    private int minScale = 1;
+    private int maxScale = 5;
+    private bool isScale = false;
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(transform.position, Vector3.up, 70 * Time.deltaTime);
+
+        if (transform.localScale.x >= maxScale - 1)
+        {
+            isScale = false;
+        }
+
+        if (transform.localScale.x <= minScale + 1)
+        {
+            isScale = true;
+        }
+
+        transform.localScale = Vector3.Lerp(
+            transform.localScale,
+            isScale 
+                ? new Vector3(maxScale, maxScale, maxScale) 
+                : new Vector3(minScale, minScale, minScale), 
+            Time.deltaTime);
+    }
+}
