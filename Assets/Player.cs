fce2098fd19740b550608bba3b1344c634f15fb1
r.lomaka <ruslan.lomaka@Gmail.com>
2021-03-15 07:54:18 +0200

    Scene 2 setup

---
+++ Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float _speed = 20f;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private Animator _animator;
+
+    private float _vertical;
+    private float _horizontal;
+
+    private Vector2 _movement;
+
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+    private static readonly int LastHorizontal = Animator.StringToHash("Last_Horizontal");
+    private static readonly int LastVertical = Animator.StringToHash("Last_Vertical");
+
+    private void Update()
+    {
+        _movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        _animator.SetFloat(Horizontal, _movement.x);
+        _animator.SetFloat(Vertical, _movement.y);
+        _animator.SetBool(IsWalking, _movement.sqrMagnitude > 0.01);
+
+        if (_movement.x > 0.1 || _movement.x < -0.1 || _movement.y > 0.1 || _movement.y < -0.1)
+        {
+            _animator.SetFloat(LastHorizontal, _movement.x);
+            _animator.SetFloat(LastVertical, _movement.y);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + _movement * (_speed * Time.fixedDeltaTime));
+    }
+}
---
+++ Assets/ProjectAssets/Scripts/Chest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    private bool isOpen = false;
+    private static readonly int IsOpen = Animator.StringToHash("IsOpen");
+
+    private void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space)) return;
+        isOpen = !isOpen;
+        animator.SetBool(IsOpen, isOpen);
+    }
+}
