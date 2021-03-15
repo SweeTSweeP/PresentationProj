c6bfea6e4aea5bdeb8ccf807bf862e6ee840b88f
r.lomaka <ruslan.lomaka@Gmail.com>
2021-03-15 22:41:52 +0200

    New scenes

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public Animator animator;
+    
+    public float speed = 6f;
+    
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        //float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(0, h, 0);
+        
+        
+        if (direction.magnitude >= 0.1f)
+        {
+            controller.Move(direction * speed * Time.deltaTime);
+            animator.SetBool("IsMoving", true);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
+        }
+    }
+}
